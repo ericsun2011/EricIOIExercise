d57afe151689bcc8728a4e598e20b2cd50623455
ericsun2011 <78456449+ericsun2011@users.noreply.github.com>
2022-03-01 18:14:08 -0500

    Create Sum.cpp

---
+++ SumTwoVariables/Sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int x;
+int y;
+
+cout << "Enter a number: \n\r";
+cin >> x;
+cout << "Enter another number: \n\r";
+cin >> y;
+cout << "Sum is: " << x + y;
+
+return 0;
+}
