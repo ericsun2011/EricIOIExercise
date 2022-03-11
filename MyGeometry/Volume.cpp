f6ed31ba61a653766ef4f8c765d5d6a872c3bcb1
ericsun2011 <78456449+ericsun2011@users.noreply.github.com>
2022-03-10 19:23:54 -0500

    Volumes

--- MyGeometry/Volume.cpp
+++ MyGeometry/Volume.cpp
@@ -1,22 +1,51 @@
 #include <iostream>
 using namespace std;
 
-double cubed(double sides);
+double cubed(double CubeSides);
+double sphere(double SphereR);
+double cylinder(double CylR, double CylH);
 
 int main() {
-
-    double sides;
-    double volume;
+    double CubeSides;
+    double CubeVolume;
+    double SphereR;
+    double SphereVolume;
+    double CylR;
+    double CylH;
+    double CylVolume;
 
     cout << "Enter a size for the sides: ";
-    cin >> sides;
-    volume = cubed(sides);
-    cout << "Volume is: " << volume << endl;
-
+    cin >> CubeSides;
+    CubeVolume = cubed(CubeSides);
+    cout << "Volume for the cube is: " << CubeVolume << endl;
+    
+    cout << "Enter radius for sphere: ";
+    cin >> SphereR;
+    SphereVolume = sphere(SphereR);
+    cout << "Volume for the sphere is: " << SphereVolume << endl;
+    
+    cout << "Enter radius for cylinder: ";
+    cin >> CylR;
+    cout << "Enter height for cylinder: ";
+    cin >> CylH;
+    CylVolume = cylinder(CylR, CylH);
+    cout << "Volume for the cylinder is: " << CylVolume << endl;
+    
     return 0;
 }
 
-double cubed(double sides){
-    double volume = sides * sides * sides;
-    return volume;
+double cubed(double CubeSides){
+    double CubeVolume = CubeSides * CubeSides * CubeSides;
+    return CubeVolume;
+}
+
+double sphere(double SphereR){
+    double x = 4, y = 3;
+    double SphereVolume = (x / y) * 3.14159 * SphereR * SphereR * SphereR;
+    return SphereVolume;
+}
+
+double cylinder(double CylR, double CylH){
+    double CylVolume = 3.14159 * CylR * CylR * CylH;
+    return CylVolume;
 }
---
+++ MyGeometry/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double x = 4, y = 3;
+    cout << x / y;
+
+    return 0;
+}
