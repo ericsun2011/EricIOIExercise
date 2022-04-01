20e12a2ac769d256b8a01cc401cad84ae0dbdc9b
ericsun2011 <78456449+ericsun2011@users.noreply.github.com>
2022-03-31 21:42:45 -0400

    Create Operators.cpp

---
+++ BinaryOperaters/Operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main() {
+
+    int x = 10, y = 6;
+
+    cout << (x & y) << endl;
+    cout << (x | y) << endl;
+    cout << (x ^ y) << endl;
+    cout << (~x) << endl;
+    cout << (x >> 2) << endl;
+    cout << (x << 2) << endl;
+
+    
+    return 0;
+}
