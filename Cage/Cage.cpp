65fdf5131213ef57c6c78c36faab05039cf833d8
ericsun2011 <78456449+ericsun2011@users.noreply.github.com>
2022-04-13 21:41:52 -0400

    wwe

---
+++ Cage/Cage.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    freopen("data.in", "r", stdin);
+    freopen("answer.out", "w" , stdout);
+
+
+
+    int c = 0, r = 0, h, f;
+
+    while(scanf("%d %d", &h, &f) == 2) {
+
+
+
+    c = (h * 4 - f) / 2;
+    r = h - c;
+    
+    
+    cout << c << " " << r << endl;
+
+    }    
+
+    fclose(stdin);
+    fclose(stdout);
+
+    return 0;
+}
+
