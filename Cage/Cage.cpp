284032fc9be2d4f82daa113e6bf99451c3d400af
ericsun2011 <78456449+ericsun2011@users.noreply.github.com>
2022-04-14 21:40:17 -0400

    t

--- Cage/Cage.cpp
+++ Cage/Cage.cpp
@@ -1,31 +1,25 @@
 #include <iostream>
 using namespace std;
 
-
-int main() {
+int main()
+{
 
     freopen("data.in", "r", stdin);
-    freopen("answer.out", "w" , stdout);
-
-
+    freopen("answer.out", "w", stdout);
 
     int c = 0, r = 0, h, f;
 
-    while(scanf("%d %d", &h, &f) == 2) {
+    while (scanf("%d %d", &h, &f) == 2)
+    {
 
+        c = (h * 4 - f) / 2;
+        r = h - c;
 
-
-    c = (h * 4 - f) / 2;
-    r = h - c;
-    
-    
-    cout << c << " " << r << endl;
-
-    }    
+        cout << c << " " << r << endl;
+    }
 
     fclose(stdin);
     fclose(stdout);
 
     return 0;
 }
-
---
+++ Fibbonacci/Sequence.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x = 0, y = 1, z, n;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        if(i <= 1){
+            z = i;
+        }
+        else if(x + y <= n){
+            z = x + y;
+            x = y;
+            y = z;
+            
+        }
+        else{
+            break;
+        }
+        cout << z << " ";
+    }
+
+    return 0;
+}
